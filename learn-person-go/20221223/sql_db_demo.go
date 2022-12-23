54650b91fbf4208116023cbb2a177603973d9201
胡宗红 <15210012646@163.com>
2022-12-23 19:03:40 +0800

    commit

--- learn-person-go/20221223/sql_db_demo.go
+++ learn-person-go/20221223/sql_db_demo.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"learn_demo/config"
-	"log"
 )
 
 //var db *sql.DB
@@ -14,23 +12,23 @@ var (
 )
 
 func main() {
-	database := config.MysqlConn()
-	//db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go_test")
-	rows, err := database.Query("select id,name from users where id = ?", 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Fatalln(id, name)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//database := config.MysqlConn()
+	////db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go_test")
+	//rows, err := database.Query("select id,name from users where id = ?", 1)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer rows.Close()
+	//for rows.Next() {
+	//	err := rows.Scan(&id, &name)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	log.Fatalln(id, name)
+	//}
+	//err = rows.Err()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 }
